1026c3fee62d3fcad5d4f31bc5a7f65e3ea0ddd9
sumanthmvs2012 <154765881+sumanthmvs2012@users.noreply.github.com>
2025-10-10 18:27:32 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_TCe7hzLa5', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'eq2d9heg401c6vqlb550lruu1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
