585dc587eab004057945002b2f951b4a41ce07b1
sumanthmvs2012 <154765881+sumanthmvs2012@users.noreply.github.com>
2025-10-11 23:46:52 -0400

    Update a change config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_TCe7hzLa5', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'eq2d9heg401c6vqlb550lruu1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_EzKxR1Ftw', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1hplnjfsqra5sauknbgpnpesnf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
